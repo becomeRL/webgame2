0bd4aff1513351e4af1ef5784365794d02d4c54c
lightningchoe <66203652+lightningchoe@users.noreply.github.com>
2020-08-03 20:55:40 +0900

    add escape function and number range

--- br33.js
+++ br33.js
@@ -21,15 +21,17 @@ br33.addEventListener('click', function(){
 
     var game = function(){
         for(var num = 0; num <= 31;){
-        choice1 = Number(prompt("현재 수는 " + num + "입니다. 더하길 원하는 수를 입력해 주세요.\n(1보다 작은 수를 입력하면 1, 3보다 큰 수를 입력하면 3으로 처리됩니다.)"));
-            if(isNaN(choice1) === true){
-                alert("숫자만 입력하셔야 합니다. 문자를 입력하셨으니 강제로 1을 더합니다.");
-                choice1 = 1;
+        choice1 = Number(prompt("현재 수는 " + num + "입니다. 더하길 원하는 수를 입력해 주세요.\n(1, 2 ,3 중 하나만 입력하고 그 외에는 게임이 종료됩니다.)"));
+            if(choice1){
+                alert(choice1+ " 을 더합니다.");                
             }
-            if(choice1 > 3){
-                choice1 = 3;
-            } else if(choice1 < 1){
-                choice1 = 1;
+            else{
+                alert("취소되었습니다");
+                return;
+            }
+            if(choice1 > 3 || choice1 < 1){
+                alert("반칙입니다");
+                return;
             }
         num += choice1;
         alert(name1 + "이/가 " + choice1 + "를 더하여 " + num + "이 되었습니다.");
@@ -40,15 +42,17 @@ br33.addEventListener('click', function(){
                 break;
             }
 
-            choice2 = Number(prompt("현재 수는 " + num + "입니다. 더하길 원하는 수를 입력해 주세요.\n(1보다 작은 수를 입력하면 1, 3보다 큰 수를 입력하면 3으로 처리됩니다.)"));
-            if(isNaN(choice2) === true){
-                alert("숫자만 입력하셔야 합니다. 문자를 입력하셨으니 강제로 1을 더합니다.");
-                choice2 = 1;
+        choice1 = Number(prompt("현재 수는 " + num + "입니다. 더하길 원하는 수를 입력해 주세요.\n(1, 2 ,3 중 하나만 입력하고 그 외에는 게임이 종료됩니다.)"));
+            if(choice1){
+                alert(choice1+ " 을 더합니다.");                
             }
-            if(choice1 > 3){
-                choice2 = 3;
-            } else if(choice2 < 1){
-                choice2 = 1;
+            else{
+                alert("취소되었습니다");
+                return;
+            }
+            if(choice1 > 3 || choice1 < 1){
+                alert("반칙입니다");
+                return;
             }
         num += choice2;
         alert(name2 + "이/가 " + choice2 + "를 더하여 " + num + "이 되었습니다.");
@@ -65,11 +69,15 @@ br33.addEventListener('click', function(){
     game();
 
     if(last === name1){
-        document.getElementById("content").innerHTML = name1 + "이/가 " + lastChoice + "을 더하여 " + lastNum + "이 되었습니다. <br>" + name2 + " 승";
-    } else{
-        document.getElementById("content").innerHTML = name2 + "이/가 " + lastChoice + "을 더하여 " + lastNum + "이 되었습니다. <br>" + name1 + " 승";
+        document.getElementById("game3").innerHTML = name1 + "이/가 " + lastChoice + "을 더하여 " + lastNum + "이 되었습니다. <br>" + name2 + " 승";
+    } else if(choice1 > 3 || choice1 < 1){
+        document.getElementById("game3").innerHTML = name1 + "이/가 " + choice1 + "을 더하여 반칙으로 처리되었습니다. <br>" + name2 + " 승";
+    }else if(choice2 > 3 || choice2 < 1){
+        document.getElementById("game3").innerHTML = name2 + "이/가 " + choice2 + "을 더하여 반칙으로 처리되었습니다. <br>" + name1 + " 승";
+    }else{
+        document.getElementById("game3").innerHTML = name2 + "이/가 " + lastChoice + "을 더하여 " + lastNum + "이 되었습니다. <br>" + name1 + " 승";
     }
 }
 )
-document.getElementById("content").innerHTML = text;
+document.getElementById("game3").innerHTML = text;
     
